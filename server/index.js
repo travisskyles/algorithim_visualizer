48343f056f3c2abce767937d98b635ebc98bd56a
travis skyles <travis.skyles@gmail.com>
2020-09-03 15:47:09 -0700

    serving docs files at /docs

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '../', 'build');
+const directory = path.join(__dirname, '..');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static(`${directory}/build`));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(`${directory}/build`, 'index.html'));
+});
+
+app.get('/docs', (req, res) => {
+	res.sendFile(path.join(`${directory}/public/docs`, 'index.html'));
 });
 
 app.listen(port, () => {
