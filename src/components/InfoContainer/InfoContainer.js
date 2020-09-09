d5563f522e04a75780eb05e810332d4ff8d4e8c9
travis skyles <travis.skyles@gmail.com>
2020-09-08 18:33:41 -0700

    made a basic key

--- src/components/InfoContainer/InfoContainer.js
+++ src/components/InfoContainer/InfoContainer.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
+import { FaStarOfLife } from 'react-icons/fa';
+import { ImCross } from 'react-icons/im';
 import './InfoContainer.css';
 
 export default function InfoContainer(props) {
 	return (
-		<div className='info-container'>
-			<div className='message_box'>
-				<p>{props.message ? props.message : ''}</p>
+		<IconContext.Provider value={{ size: '20px', className: 'icon' }}>
+			<div className='info-container'>
+				<div className='message_box'>
+					<p id='message-text'>{props.message ? props.message : ''}</p>
+				</div>
+				<div className='key'>
+					<div>
+						<div className='key-item-wrapper'>
+							<p className='key-text'>Start - </p>
+							<FaStarOfLife />
+						</div>
+						<div className='key-item-wrapper'>
+							<p className='key-text'>Finish- </p>
+							<ImCross />
+						</div>
+					</div>
+					<div>
+						<div className='key-item-wrapper'>
+							<p className='key-text'>Walls - </p>
+							<div id='wall-key'></div>
+						</div>
+						<p className='key-text'>
+							Weights - Hold 'W' and Click to add weight
+						</p>
+					</div>
+				</div>
 			</div>
-			<div className='key'></div>
-		</div>
+		</IconContext.Provider>
 	);
 }
