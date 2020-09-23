8ffc60a0a3fed1269c0d83d21998f275ce3a5ac6
travis skyles <travis.skyles@gmail.com>
2020-09-22 18:03:18 -0700

    breadth first algorithm created

---
+++ src/algorithims/breadthFirst.js
@@ -0,0 +1,71 @@
+export const breadthFirst = {
+	run: function (grid, startNode, finishNode) {
+		// ensure all parameter exist and start and finish are not the same
+		if (!grid || !startNode || !finishNode || startNode === finishNode) {
+			return false;
+		}
+		// create visited an unvisited lists
+		const visited = [];
+		const unvisited = [];
+		// set startNode distance to 0 and put into unvisited list
+		startNode.distance = 0;
+		unvisited.push(startNode);
+
+		while (!!unvisited.length) {
+			// sort the unvisited nodes from smallest to largest
+			// set the current node as the first from the array
+			this._sortNodesByDistance(unvisited);
+			let current = unvisited.shift();
+
+			if (current === finishNode) return visited;
+			if (current.isWall) continue;
+
+			// set current as visited
+			// push current into visited list
+			visited.push(current);
+
+			// get the neighbors of current
+			// then do stuff with neighbors
+			let neighbors = this._getNeighbors(current, grid);
+			for (let neighbor of neighbors) {
+				if (neighbor.isWall || visited.includes(neighbor)) {
+					continue;
+				}
+				if (!unvisited.includes(neighbor)) {
+					unvisited.push(neighbor);
+				}
+			}
+		}
+		return [];
+	},
+
+	/**
+	 * finds the neighbors of a node in 4 directions
+	 * @function _getNeighbors
+	 * @private
+	 * @param {node} node a node in a matrix
+	 * @param {matrix} grid a matrix of nodes
+	 * @returns {array} a list of the 4 neighbor nodes of the inputted node
+	 */
+	_getNeighbors: function (node, grid) {
+		const neighbors = [];
+		const { column, row } = node;
+		if (row > 0) neighbors.push(grid[row - 1][column]);
+		if (row < grid.length - 1) neighbors.push(grid[row + 1][column]);
+		if (column > 0) neighbors.push(grid[row][column - 1]);
+		if (column < grid[0].length - 1) neighbors.push(grid[row][column + 1]);
+
+		return neighbors;
+	},
+
+	/**
+	 * sorts a list of nodes by their f(x) values
+	 * @function _sortNodesByDistance
+	 * @private
+	 * @param {array} arr a list of nodes
+	 * @returns {array} sorted array
+	 */
+	_sortNodesByDistance: function (arr) {
+		return arr.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+	},
+};
